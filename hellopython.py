e3dc42fe6e3ef0403efc7b1ae671ae465d03753f
yosuayos <yosua.tomogandhi@gmail.com>
2023-04-19 10:47:19 +0700

    menambahkan file hellopython.py

---
+++ hellopython.py
@@ -0,0 +1,22 @@
+# print("hello world!")
+
+# def fungsiku():
+#     print("manggil fungsi")
+
+# fungsiku()
+
+# print("Nama:",input("Siapa Nama kamu ?"))
+
+# nama = input("siapa nama kamu?")
+# negara = input("darimana negara kamu?")
+# nilai = input("berapa nilai kamu?")
+
+# print("{:<10}{:<10}{:<10}".format("nama","negara","nilai"))
+# print("{:<10}{:<10}{:<10}".format(nama,negara,nilai))
+
+#manipulasi string
+namalengkap = input("masukan nama anda: ")
+alamat = input("masukan alamat anda: ")
+kodepos = input("masukan kodepos anda: ")
+datapengirim = (namalengkap+", "+alamat+", "+kodepos)
+print(datapengirim)
