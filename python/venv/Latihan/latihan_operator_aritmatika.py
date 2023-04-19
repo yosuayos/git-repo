d206758603dc03ba4a4d837fbe2e72a7cbff8c26
yosuayos <yosua.tomogandhi@gmail.com>
2023-04-19 12:11:00 +0700

    membukan komen di soal nomor 1

--- python/venv/Latihan/latihan_operator_aritmatika.py
+++ python/venv/Latihan/latihan_operator_aritmatika.py
@@ -1,9 +1,9 @@
-# kelas_5c = 40
-# matematika = 19
-# inggris = 24
-# keduanya = 15
-# tidak_keduanya = kelas_5c - keduanya
-# print(tidak_keduanya)
+kelas_5c = 40
+matematika = 19
+inggris = 24
+keduanya = 15
+tidak_keduanya = kelas_5c - keduanya
+print(tidak_keduanya)
 
 # 2 buku 1 pensil
 a = 6000
